cae7a68cc58479280e2517d3e3fc0d1e008292b9
orhiley90 <69226157+orhiley90@users.noreply.github.com>
2020-09-19 21:35:12 -0700

    Got it to show the hours and no longer returns an error in the console when the save button is clicked

--- script.js
+++ script.js
@@ -1,31 +1,58 @@
 $(document).ready(function () {
+    let currentDate = moment().format("MMM Do YYYY");
+    // shows current date in the header
+    $(".currentDate").append(currentDate);
 
-    let currentTime = moment().hours();
+    // save button that saves content to local storage
+    $(".saveLocal").on("click", function () {
+        let id = this.id;
 
-    $(".saveLocal").on("click", function()  {
-        console.log("This is working button!");
+        localStorage.setItem($("#textArea" + id), $("#textArea" + id).val());
+        console.log($("#textArea" + id).val());
+        console.log(localStorage.getItem($("textArea" + id).val()));
+
+    });
+// this changes the color of the current block to show what is currently on the docket
+    function updateHour() {
+
+        let currentHour = moment().hours();
+
+        $(".colorBlock").each(function () {
+
+            $(this).removeClass("present");
+
+            let blockHour = $(this).id;
+            
+            if (blockHour === currentHour) {
+                $(this).addClass("present");
+
+            }
+        });
+    }
+
+    updateHour();
 
 
-
-    }); 
+    // let reloadedText = document.getElementsByClassName("textLoader");
+    // console.log("textArea" + id);
+    localStorage.getItem("textArea9");
+    $("#textArea9 .textLoader").val(localStorage.getItem("textArea9"));
+    localStorage.getItem("textArea10");
+    $("#textArea10 .textLoader").val(localStorage.getItem("textArea10"));
+    localStorage.getItem("textArea11");
+    $("#textArea11 .textLoader").val(localStorage.getItem("textArea11"));
+    localStorage.getItem("textArea12");
+    $("#textArea12 .textLoader").val(localStorage.getItem("textArea12"));
+    localStorage.getItem("textArea1");
+    $("#textArea1 .textLoader").val(localStorage.getItem("textArea1"));
+    localStorage.getItem("textArea2");
+    $("#textArea2 .textLoader").val(localStorage.getItem("textArea2"));
+    localStorage.getItem("textArea3");
+    $("#textArea3 .textLoader").val(localStorage.getItem("textArea3"));
+    localStorage.getItem("textArea4");
+    $("#textArea4 .textLoader").val(localStorage.getItem("textArea4"));
+    localStorage.getItem("textArea5");
+    $("#textArea5 .textLoader").val(localStorage.getItem("textArea5"));
     
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 });
+
